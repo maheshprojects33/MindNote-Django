231c13c01aaec7506d4fe0541aec0d44c8a13296
maheshprojects33 <65019077+maheshprojects33@users.noreply.github.com>
2023-04-11 21:26:31 +0545

    Update settings.py
    removed secrete key

--- MindNote/settings.py
+++ MindNote/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-idnt4ou)v827z$!yfhwi^6j&udz^2v8)44##th_zvmwqadb^8q'
+SECRET_KEY = 'django-insecure'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
